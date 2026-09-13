8befcaaf2fdac8b8101cdbec594d681d2af57b4d
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2026-09-13 21:17:45 +0530

    Added fibonaci

---
+++ p00.py
@@ -0,0 +1,35 @@
+```python
+import sys
+from collections import *
+
+
+def solve(n):
+    dp=[-1]*(n+1)
+
+    def helper(n):
+        if n==0 or n==1:
+            dp[n]=n
+            return n
+
+        if dp[n]!=-1:
+            return dp[n]
+
+        dp[n]=helper(n-1)+helper(n-2)
+        return dp[n]
+
+    ans=helper(n)
+    print(dp)
+    return ans
+
+
+def main():
+    sys.stdin=open("input.txt","r")
+    sys.stdout=open("output.txt","w")
+
+    n=int(input())
+    print(solve(n))
+
+
+if __name__=="__main__":
+    main()
+```
